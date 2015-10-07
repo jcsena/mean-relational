0a774bbf532f0b4adf94dfff8cf9c40bd9f66782
Julio Sena <julio.657.js@gmail.com>
2015-10-07 16:34:59 -0500

    bug fixed and add cluster aplications

--- app.js
+++ app.js
@@ -1,32 +1,71 @@
+
+'use strict';
 /**
  * Module dependencies.
  */
 var express     = require('express');
-
-/**
- * Main application entry file.
- * Please note that the order of loading is important.
- */
-
-// Load Configurations
-
+var cluster = require('cluster');
 var env             = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var config          = require('./config/config');
 var db              = require('./config/sequelize');
 var passport        = require('./config/passport');
 var winston         = require('./config/winston');
-
-// console.log(db);
-
-var app = express();
-
-//Initialize Express
-require('./config/express')(app, passport, db);
-
-//Start the app by listening on <port>
 var port = process.env.PORT || config.port;
-app.listen(port);
-winston.info('Express app started on port ' + port);
 
-//expose app
-exports = module.exports = app;
+
+
+if(env === 'development'){
+  return createApp();
+}
+
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        cluster.fork();
+    }
+
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
+
+        // Replace the dead worker, we're not sentimental
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
+
+    });
+
+    // As workers come up.
+    cluster.on('listening', function(worker, address) {
+      console.log('A worker with #'+worker.id+' is now connected to ' + address.address + ':' + address.port);
+    });
+
+    // Count requestes
+    Object.keys(cluster.workers).forEach(function(id) {
+      cluster.workers[id].on('message', function(){
+        console.log('message');
+      });
+    });
+
+// Code to run if we're in a worker process
+} else {
+  createApp();
+}
+
+function createApp(){
+
+//Initialize database models
+  db.init(function(){
+    var app = express();
+
+    //Initialize Express
+    require('./config/express')(app, passport, db);
+
+    app.listen(port);
+    winston.info('Express app started on port ' + port);
+
+  });
+}
--- config/sequelize.js
+++ config/sequelize.js
@@ -9,70 +9,75 @@ var winston   = require('./winston');
 var db        = {};
 
 
-winston.info('Initializing Sequelize...');
-
-// create your instance of sequelize
-var sequelize = new Sequelize(config.db.name, config.db.username, config.db.password, {
-    host: config.db.host,
-    port: config.db.port,
-    dialect: 'mysql',
-    storage: config.db.storage,
-    logging: config.enableSequelizeLog ? winston.verbose : false,
-    timezone : config.timezone,
-    define: {
-        timestamps: true,
-        freezeTableName: true,
-        underscored: true
-    }
-});
-
-// loop through all files in models directory ignoring hidden files and this file
+var models = module.exports = {
+  Sequelize: Sequelize,
+  init: init
+};
 
 
-  config.getDirectories(config.root + '/packages').forEach(function(pack){
-    var pt = config.root + '/packages/' + pack + '/server/models';
-      if(fs.existsSync(pt)){
-            readdirSync(pt);
+function init(callback){
+
+  winston.info('Initializing Sequelize...');
+
+  // create your instance of sequelize
+  var sequelize = new Sequelize(config.db.name, config.db.username, config.db.password, {
+      host: config.db.host,
+      port: config.db.port,
+      dialect: 'mysql',
+      storage: config.db.storage,
+      logging: config.enableSequelizeLog ? winston.verbose : false,
+      timezone : config.timezone,
+      define: {
+          timestamps: true,
+          freezeTableName: true,
+          underscored: true
       }
   });
 
+  // loop through all files in models directory ignoring hidden files and this file
 
-// invoke associations on each of the models
-Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].options.hasOwnProperty('associate')) {
-    db[modelName].options.associate(db);
+
+    config.getDirectories(config.root + '/packages').forEach(function(pack){
+      var pt = config.root + '/packages/' + pack + '/server/models';
+        if(fs.existsSync(pt)){
+              readdirSync(pt);
+        }
+    });
+
+
+  // invoke associations on each of the models
+  Object.keys(db).forEach(function(modelName) {
+    if (db[modelName].options.hasOwnProperty('associate')) {
+      db[modelName].options.associate(db);
+    }
+  });
+
+  // Synchronizing any model changes with database.
+  // WARNING: this will DROP your database everytime you re-run your application
+  sequelize
+    .sync({force: config.forceSequelizeSync})
+    .then(function(){
+          winston.info('Database '+(config.forceSequelizeSync?'*DROPPED* and ':'')+ 'synchronized');
+          models.sequelize = sequelize;
+          _.extend(models,db);
+          callback();
+      }).catch(function(err){
+          winston.error('An error occured: %j',err);
+      });
+
+  //function create models
+
+  function readdirSync(route){
+
+    fs.readdirSync(route)
+      .filter(function(file) {
+        return (file.indexOf('.') !== 0) && (file !== 'index.js');
+      })
+      // import model files and save model names
+      .forEach(function(file) {
+        winston.info('Loading model file ' + file);
+        var model = sequelize.import(path.join(route, file));
+        db[model.name] = model;
+      });
   }
-});
-
-// Synchronizing any model changes with database.
-// WARNING: this will DROP your database everytime you re-run your application
-sequelize
-  .sync({force: config.forceSequelizeSync})
-  .then(function(){
-        winston.info('Database '+(config.forceSequelizeSync?'*DROPPED* and ':'')+ 'synchronized');
-    }).catch(function(err){
-        winston.error('An error occured: %j',err);
-    });
-
-//function create models
-
-function readdirSync(route){
-
-  fs.readdirSync(route)
-    .filter(function(file) {
-      return (file.indexOf('.') !== 0) && (file !== 'index.js');
-    })
-    // import model files and save model names
-    .forEach(function(file) {
-      winston.info('Loading model file ' + file);
-      var model = sequelize.import(path.join(route, file));
-      db[model.name] = model;
-    });
 }
-
-
-// assign the sequelize variables to the db object and returning the db.
-module.exports = _.extend({
-  sequelize: sequelize,
-  Sequelize: Sequelize
-}, db);
